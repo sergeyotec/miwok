1a56fb379a49ca9be1d94707a2ee351ebda48715
sergeyotec <ssl75@mail.ru>
2018-05-16 22:58:43 +0300

    Добавили Tabs для ViewPager

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,14 +1,21 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
+    //добавляем контекст для того чтобы для задания названий вкладок
+    //использовать строковые ресурсы
+    private Context mContext;
+
     //менеджер фрагментов, который будет сохранять состояние каждого фрагмента в адаптере через прокрутку
-    public CategoryAdapter(FragmentManager fragmentManager){
+    //еще добавляем контекст чтобы в названии вкладок использов. строковые ресурсы
+    public CategoryAdapter(Context context, FragmentManager fragmentManager){
         super(fragmentManager);
+        mContext = context;
     }
     //возвращает фрагмент который должен отображаться для данного номера страницы
     @Override
@@ -26,5 +33,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
